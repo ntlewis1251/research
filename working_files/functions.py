15052e2c02a694ce282219355ece843e24a7a84c
ntlewis1251 <ntlewis@astral.sciclone.wm.edu>
2025-04-04 14:55:56 -0400

    cleaned up a bit and created a function to fetch my api keys in notebooks and scripts

--- working_files/functions.py
+++ working_files/functions.py
@@ -1,1 +1,12 @@
-print('Success!')
+def get_key(filename:str, line:int)->str:
+    """
+    Goes into my super-secret api text file and gets my keys yurrr.
+
+    args: Filename (str), line in file (int)
+
+    returns: key
+
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return lines[line-1]
