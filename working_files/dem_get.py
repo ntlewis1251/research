2ee38b2e72c9245c45a1f15328289d7b3c58b834
ntlewis1251 <ntlewis@astral.sciclone.wm.edu>
2025-04-06 21:31:34 -0400

    Finally got the dem getting script to work.

--- working_files/dem_get.py
+++ working_files/dem_get.py
@@ -6,15 +6,16 @@ def make_dir(name):
     return '/sciclone/home/ntlewis/research/working_files/data/' + name
 
 def get_topo(bounds:list, dir:str):
-    keys = ['west','south','east','north']
+    keys = ['north','east','south','west']
+    bounds=[bounds[0],bounds[1],str(float(bounds[0])-0.125),str(float(bounds[1])-0.125)]
     dic = dict(zip(keys, bounds))
-    url = f'https://portal.opentopography.org/API/usgsdem?datasetName=USGS1m&south={dic["south"]}&north={dic["north"]}&west={dic["west"]}&east={dic["east"]}&outputFormat=GTiff&API_Key={get_key("/sciclone/home/ntlewis/research/API_key.txt", 2)}'
+    url = f'https://portal.opentopography.org/API/usgsdem?datasetName=USGS10m&south={dic["south"]}&north={dic["north"]}&west={dic["west"]}&east={dic["east"]}&outputFormat=GTiff&API_Key={get_key("/sciclone/home/ntlewis/research/API_key.txt", 2)}'
     response = requests.get(url)
     with open(dir,'wb') as file:
         file.write(response.content)
 
 def main():
-    bounds = input('Enter bounding coordinates in the following format: "west, south, east, north": ').split(',')
+    bounds = input('Enter NE bounding coordinates in the following format: "north,east": ').split(',')
     name = input('Name of where new tiff will be stored: ')
     dir = make_dir(name=name)
     get_topo(bounds=bounds, dir=dir)
